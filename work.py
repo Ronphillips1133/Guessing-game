3e4beb8c9d20be10bd3e8cae55fbea2e9b7b191d
Ronphillips1133 <77497956+Ronphillips1133@users.noreply.github.com>
2021-01-15 17:07:15 -0800

    Add files via upload

---
+++ work.py
@@ -0,0 +1,35 @@
+secert_word = ""
+guess = ""
+guess_count = 0
+guess_limit = 3
+guess_hint = 1
+third_try = 2
+guess_help = ''
+second_guess =1
+out_of_guesses = False
+numbers = ''
+enter_secert = input('Enter Secert Word: ')
+secert_word = enter_secert
+
+
+while guess != secert_word and not (out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Guess what word I am Thinking? ")
+        if guess_count > guess_count:
+            guess = input("Try Again")
+        if guess_count >= guess_hint:
+            guess = guess_help = print('Here is the First Letter:' + secert_word[0])
+        if guess_count >= third_try:
+                guess = guess_help = print('Here is the Next Letter:' + secert_word[0:2])
+        if guess_count >= guess_hint:
+            guess = input("Try again: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Good Try you LOSE! Secert Word:" + secert_word)
+else:
+    print("You Got It!")
+
+
