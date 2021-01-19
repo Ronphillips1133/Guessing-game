6ad06aa769c94f336851ecdbf7e8b865a08d8632
Ronphillips1133 <77497956+Ronphillips1133@users.noreply.github.com>
2021-01-18 18:31:13 -0800

    Update work.py

--- work.py
+++ work.py
@@ -5,14 +5,19 @@ guess_limit = 3
 guess_hint = 1
 third_try = 2
 guess_help = ''
-second_guess =1
+second_guess = 1
 numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 out_of_guesses = False
-numbers = ''
 enter_secert = input('Enter Secert Word: ')
 secert_word = enter_secert
-for numbers in number:
-    print = (numbers * '*')
+sentence = "Get Ready To Start"
+char_freq = {}
+
+
+for char in sentence:
+    if char in char_freq:
+        char_freq += 1
+
 
 while guess != secert_word and not (out_of_guesses):
     if guess_count < guess_limit:
@@ -20,9 +25,11 @@ while guess != secert_word and not (out_of_guesses):
         if guess_count > guess_count:
             guess = input("Try Again")
         if guess_count >= guess_hint:
-            guess = guess_help = print('Here is the First Letter:' + secert_word[0])
+            guess = guess_help = print(
+                'Here is the First Letter:' + secert_word[0])
         if guess_count >= third_try:
-                guess = guess_help = print('Here is the Next Letter:' + secert_word[0:2])
+            guess = guess_help = print(
+                'Here is the Next Letter:' + secert_word[0:2])
         if guess_count >= guess_hint:
             guess = input("Try again: ")
         guess_count += 1
@@ -33,5 +40,3 @@ if out_of_guesses:
     print("Good Try you LOSE! Secert Word:" + secert_word)
 else:
     print("You Got It!")
-
-
